c8c0f00b597fde28840c66e4586a99a5554f3b47
Cythilya Tang <cythilya@gmail.com>
2023-09-07 16:53:21 +0800

    feat: add map sample

--- src/features/searchLocation/SearchLocation.js
+++ src/features/searchLocation/SearchLocation.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   AzureMap,
   AzureMapsProvider,
-  IAzureMapOptions,
   AuthenticationType
 } from 'react-azure-maps';
+import MapController from './MapController';
+// import styles from './SearchLocation.module.css';
 
 const option = {
+  center: [-74.0060, 40.7128],
+  zoom: 14,
+  view: 'Auto',
   authOptions: {
-    authType: AuthenticationType.subscriptionKey,
     subscriptionKey: process.env['REACT_APP_SUBSCRIPTION_KEY'],
+    authType: AuthenticationType.subscriptionKey,
   },
 };
 
 const SearchLocation = () => {
+  let minSearchInputLength = 3; // The minimum number of characters needed in the search input before a search is performed.
+  let keyStrokeDelay = 150; // The number of ms between key strokes to wait before performing a search.
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <AzureMapsProvider>
-    <div style={{ height: '300px' }}>
-      <AzureMap options={option} />
-    </div>
+      <div style={{ height: '300px' }}>
+        <AzureMap options={option} />
+        <MapController />
+      </div>
   </AzureMapsProvider>
   );
 };
 
 export default SearchLocation;
+
+// ref: https://codesandbox.io/s/map-forked-6nbwi?file=/src/App.js
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './app/store';
 import './index.css';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
